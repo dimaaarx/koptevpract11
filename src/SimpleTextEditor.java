c1705488ba2701d8f81d13895822d97e97ef406c
dimaaarx <koptevdima1107@gmail.com>
2025-04-03 21:46:27 +0300

    Update SimpleTextEditor.java

--- src/SimpleTextEditor.java
+++ src/SimpleTextEditor.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*; import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class SimpleTextEditor {
+
+ public static void main(String[] args)
+ {
+ Scanner scanner = new Scanner(System.in); while (true)
+ {
+ System.out.println("Меню:");
+ System.out.println("1. Записати в файл");
+ System.out.println("2. Прочитати файл");
+ System.out.println("3. Вихід");
+ System.out.print("Выберить дію: ");
+
+    int choice = scanner.nextInt();
+    scanner.nextLine();
+
+    if (choice == 1) {
+        writeToFile(scanner);
+    } else if (choice == 2) {
+        readFromFile();
+    } else if (choice == 3) {
+        System.out.println("Вихід із програми.");
+        break;
+    } else {
+        System.out.println("Невірний ввід. Спробуйте ще раз .");
+    }
+}
+    scanner.close();
+}
+
+    public static void writeToFile(Scanner scanner) {
+        System.out.print("Введіть строку для запису в файл: ");
+        String text = scanner.nextLine();
+
+        try (FileWriter writer = new FileWriter("data.txt", true)) {
+            writer.write(text + "\n");
+            System.out.println("Запис виконан успішно.");
+        } catch (IOException e) {
+            System.out.println("Помилка при запису в файл.");
         }
     }
+
+    public static void readFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            System.out.println("Вміст файлу:");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не знайден. Спочатку додайте дані");
+        } catch (IOException e) {
+            System.out.println("Помилка при читання  файлу.");
+        }
+    }
+
 }
